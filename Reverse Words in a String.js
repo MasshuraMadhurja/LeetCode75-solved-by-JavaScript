78b202e1544c69e80a78b2690db74f0e7c49889d
Masshura Mayasir Madhurja <83574137+MasshuraMadhurja@users.noreply.github.com>
2024-05-27 05:25:02 +0600

    Update Reverse Words in a String.js

--- Reverse Words in a String.js
+++ Reverse Words in a String.js
@@ -1,1 +1,16 @@
+var reverseWords = function(s) {
+    let arr=[];
+    let w=[]
+    let result="";
+     w=s.split(" ").filter(word => word);
+    for(i=0;i<w.length;i++){
+      if(w[i]==w.length)
+        {result+=w[w.length-1-i]
+    }else{
+        result+=w[w.length-1-i]+" "
+    } 
+        
+    }
 
+    return result.trim();
+};
