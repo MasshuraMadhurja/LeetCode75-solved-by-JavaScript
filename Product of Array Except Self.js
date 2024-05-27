b9a73b4318651e3c7e4c4d22a93b3e973b21c93f
Masshura Mayasir Madhurja <83574137+MasshuraMadhurja@users.noreply.github.com>
2024-05-27 20:44:09 +0600

    Update Product of Array Except Self.js

--- Product of Array Except Self.js
+++ Product of Array Except Self.js
@@ -1,1 +1,20 @@
+function productExceptSelf(nums) {
+    const length = nums.length;
+    const answer = new Array(length).fill(1);
 
+    // First pass: Calculate prefix products (left products)
+    let leftProduct = 1;
+    for (let i = 0; i < length; i++) {
+        answer[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+
+    // Second pass: Calculate suffix products (right products) and multiply
+    let rightProduct = 1;
+    for (let i = length - 1; i >= 0; i--) {
+        answer[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return answer;
+}
