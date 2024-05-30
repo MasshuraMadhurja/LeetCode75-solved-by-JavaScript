ffdcb5616a06a9cd95ba6a99f07d9e54be037208
Masshura Mayasir Madhurja <83574137+MasshuraMadhurja@users.noreply.github.com>
2024-05-30 23:48:18 +0600

    Update Increasing Triplet Subsequence.js

--- Increasing Triplet Subsequence.js
+++ Increasing Triplet Subsequence.js
@@ -1,1 +1,17 @@
 
+var increasingTriplet = function (nums) {
+    let min = nums[0];
+
+    let min2 = Number.MAX_SAFE_INTEGER;
+
+    for (var i = 1; i < nums.length; i++) {
+        if (nums[i] <= min) {
+            min = nums[i];
+        } else if (nums[i] <= min2) {
+            min2 = nums[i];
+        } else {
+            return true;
+        }
+    }
+    return false;
+};
