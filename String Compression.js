f6d1c7b29c78973262e85b691f4374b860c6bef8
Masshura Mayasir Madhurja <83574137+MasshuraMadhurja@users.noreply.github.com>
2024-05-30 23:49:37 +0600

    Update String Compression.js

--- String Compression.js
+++ String Compression.js
@@ -1,1 +1,24 @@
-
+var compress = function (chars) {
+    let s = chars[0];
+    let count = 1;
+    for (let i = 1; i < chars.length; i++) {
+        if (chars[i] === chars[i - 1]) {
+            count++;
+        } else {
+            if (count > 1) {
+                s += count;
+               
+            }
+            s += chars[i];
+            count = 1;
+            
+        }
+        if (i === chars.length - 1 && count > 1) {
+            s += count;
+        }
+    }
+     for (let i = 0; i < s.length; i++) {
+         chars[i] = s[i];
+    }
+   return s.length;
+};
