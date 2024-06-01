20a5b03ceec64f9ba5fcdff45a81c6eac3067fe6
Masshura Mayasir Madhurja <83574137+MasshuraMadhurja@users.noreply.github.com>
2024-06-01 23:56:54 +0600

    Update Is Subsequence.js

--- Is Subsequence.js
+++ Is Subsequence.js
@@ -1,1 +1,11 @@
+var isSubsequence = function(s, t) {
+    let i = 0; // Pointer for s
 
+    for (let j = 0; j < t.length; j++) {
+        if (i < s.length && s[i] === t[j]) {
+            i++;
+        }
+    }
+
+    return i === s.length;
+};
